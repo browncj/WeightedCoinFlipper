508828a44fd76a62c9de790e82c6ba513efcfc21
Christopher Brown <cbrown119955@gmail.com>
2015-10-25 23:04:32 -0700

    Add settings activity

--- app/src/main/java/org/bitbucket/guruchris/weightedcoinflipper/MainActivity.java
+++ app/src/main/java/org/bitbucket/guruchris/weightedcoinflipper/MainActivity.java
@@ -1,5 +1,6 @@
 package org.bitbucket.guruchris.weightedcoinflipper;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -77,6 +78,8 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
             return true;
         }
 
